9d3e54c080b13bcb24159e53d86157729eec491b
RohitKushvaha <oldisg131@gmail.com>
2024-06-10 09:35:52 +0530

    Update workflows

---
+++ .github/workflows/newRelease.sh
@@ -0,0 +1,3 @@
+curl -X POST -H 'Content-Type: application/json' \
+  -d '{"message_thread_id": "8","chat_id": "-1002225667339_8", "text": "New Version just Released"' \
+  https://api.telegram.org/bot$TOKEN/sendMessage
