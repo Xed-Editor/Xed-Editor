e4f9af222d0a8bf4514cca87f1750db4f01ea368
RohitKushvaha <oldisg131@gmail.com>
2024-06-10 12:08:12 +0530

    Update newCommit.sh

--- .github/workflows/newCommit.sh
+++ .github/workflows/newCommit.sh
@@ -1,3 +1,4 @@
+echo $TOKEN
 curl -X POST -H 'Content-Type: application/json' \
                           -d '{"message_thread_id": "8","chat_id": "-1002225667339_8", "text": "New <a href='"$COMMIT_URL"'>Commit</a> by <a href=\"https://github.com/'"$USER"'\">@'"$USER"'</a>\n", "parse_mode": "HTML"}' \
                           https://api.telegram.org/bot$TOKEN/sendMessage
