994a5a4f9c4c73cabe0600d57fb64cf3e61a3c3b
RohitKushvaha <oldisg131@gmail.com>
2024-06-10 12:39:37 +0530

    Update newCommit.sh

--- .github/workflows/newCommit.sh
+++ .github/workflows/newCommit.sh
@@ -1,5 +1,15 @@
-export MSG=$(git log -1 --pretty=%B)
-curl -X POST -H 'Content-Type: application/json' \
-                          -d '{"message_thread_id": "8","chat_id": "-1002225667339_8", "text": "New <a href='"$COMMIT_URL"'>Commit</a> by <a href=\"https://github.com/'"$USER"'\">@'"$USER"'</a>\nDiscription: '"$MSG"'", "parse_mode": "HTML"}' \
-                          https://api.telegram.org/bot$TOKEN/sendMessage
+# Define the JSON payload separately for clarity
+payload='{
+    "message_thread_id": "8",
+    "chat_id": "-1002225667339_8",
+    "text": "New <a href=\"'$COMMIT_URL'\">Commit</a> by <a href=\"https://github.com/'$USER'\">@'$USER'</a>\nDescription: '$MSG'",
+    "parse_mode": "HTML"
+}'
+
+# Use the payload variable in the curl command
+curl -X POST \
+     -H 'Content-Type: application/json' \
+     -d "$payload" \
+     "https://api.telegram.org/bot$TOKEN/sendMessage"
+
 
