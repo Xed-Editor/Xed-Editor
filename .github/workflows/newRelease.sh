3fc501b22d041e682d976ad13a77c7a9d9f0e845
RohitKushvaha <oldisg131@gmail.com>
2024-06-10 09:41:47 +0530

    Update newRelease.sh

--- .github/workflows/newRelease.sh
+++ .github/workflows/newRelease.sh
@@ -1,3 +1,3 @@
 curl -X POST -H 'Content-Type: application/json' \
-  -d '{"message_thread_id": "10","chat_id": "-1002225667339_10", "text": "New Version just Released"' \
+  -d '{"message_thread_id": "10","chat_id": "-1002225667339_10", "text": "New Version $VERSION just Released Download Here $URL"' \
   https://api.telegram.org/bot$TOKEN/sendMessage
