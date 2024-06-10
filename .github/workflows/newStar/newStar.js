209185fdf46dad82f4c6be127443200295d94c3c
RohitKushvaha01 <oldisg131@gmail.com>
2024-06-10 08:42:27 +0530

    added star notification to telegram group

--- .github/workflows/newStar/newStar.js
+++ .github/workflows/newStar/newStar.js
@@ -7,4 +7,5 @@ const bot = new TelegramBot(token, { polling: true });
 const chatId = '-1002225667339';
 const actor = process.env.GITHUB_ACTOR;
 // Send a message to the group chat
-bot.sendMessage(chatId, "⭐ <a href=\"example.com\">@"+actor+"</a> Starred <a href=\"example.com\">Xed-Editor"+"</a>");
+bot.sendMessage(chatId, "⭐ <a href=\"http://example.com\">@"+actor+"</a> Starred <a href=\"http://example.com\">Xed-Editor</a>", { parse_mode: "HTML" });
+process.exit();
