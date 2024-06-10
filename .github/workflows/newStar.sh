ca33fa7c832f1df29ba0d96c97682e7837089360
RohitKushvaha <oldisg131@gmail.com>
2024-06-10 09:27:43 +0530

    Update newStar.sh

--- .github/workflows/newStar.sh
+++ .github/workflows/newStar.sh
@@ -1,3 +1,3 @@
 curl -X POST -H 'Content-Type: application/json' \
-  -d '{"chat_id": "-1002225667339_8", "text": "⭐ <a href=\"https://github.com/'"$actor"'\">@'"$actor"'</a> Starred <a href=\"http://example.com\">Xed-Editor</a>", "parse_mode": "HTML"}' \
+  -d '{"chat_id": "-1002225667339_8", "text": "⭐ <a href=\"https://github.com/'"$USER"'\">@'"$USER"'</a> Starred <a href=\"http://example.com\">Xed-Editor</a>", "parse_mode": "HTML"}' \
   https://api.telegram.org/bot$TOKEN/sendMessage
