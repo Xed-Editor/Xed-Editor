257a81b42c8d4981b1bc6104f2f94ad7ce5dd2ef
RohitKushvaha <oldisg131@gmail.com>
2024-06-10 09:19:00 +0530

    Update workflows

---
+++ .github/workflows/newStar.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Content-Type: application/json'   -d '{"message_thread_id": "8", "chat_id": "-1002225667339_8", "text": "This is a test message from the alert system. Do not pay attention on it"}'   https://api.telegram.org/bot$TOKEN/sendMessage
