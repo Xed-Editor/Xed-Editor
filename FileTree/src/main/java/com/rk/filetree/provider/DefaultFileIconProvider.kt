79afaf1625f8f040c8a7c7726ed9fef95d833773
Andro_Dev <rudenko2007artem@gmail.com>
2024-08-31 20:24:14 +0300

    Added file icons for language files

--- FileTree/src/main/java/com/rk/filetree/provider/DefaultFileIconProvider.kt
+++ FileTree/src/main/java/com/rk/filetree/provider/DefaultFileIconProvider.kt
@@ -16,7 +16,20 @@ class DefaultFileIconProvider(context: Context) : FileIconProvider {
 
     override fun getIcon(node: Node<FileObject>): Drawable? {
         return if (node.value.isFile()){
-            file
+            when (node.value.getName().substringAfterLast('.', "")) {
+                "java", "bsh" -> ContextCompat.getDrawable(context, R.drawable.ic_language_java)
+                "html" -> ContextCompat.getDrawable(context, R.drawable.ic_language_html)
+                "kt", "kts" -> ContextCompat.getDrawable(context, R.drawable.ic_language_kotlin)
+                "py" -> ContextCompat.getDrawable(context, R.drawable.ic_language_python)
+                "xml" -> ContextCompat.getDrawable(context, R.drawable.ic_language_xml)
+                "js" -> ContextCompat.getDrawable(context, R.drawable.ic_language_js)
+                "c" -> ContextCompat.getDrawable(context, R.drawable.ic_language_c)
+                "cpp", "h" -> ContextCompat.getDrawable(context, R.drawable.ic_language_cpp)
+                "json" -> ContextCompat.getDrawable(context, R.drawable.ic_language_json)
+                "css" -> ContextCompat.getDrawable(context, R.drawable.ic_language_css)
+                "cs" -> ContextCompat.getDrawable(context, R.drawable.ic_language_csharp)
+                else -> file
+            }
         }else{
             folder
         }
