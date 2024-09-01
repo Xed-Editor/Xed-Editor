ef52043a8748f2470cb8dae0713c7c156bda0327
Andro_Dev <87223939+AndroMaster436@users.noreply.github.com>
2024-09-01 08:47:08 +0300

    Icons and syntaxes (#105)
    * Added file icons for language files
    
    * Try to find error
    
    * Fix compile error
    
    * Add yaml syntaxes
    
    * Remove markdown file icon

--- FileTree/src/main/java/com/rk/filetree/provider/DefaultFileIconProvider.kt
+++ FileTree/src/main/java/com/rk/filetree/provider/DefaultFileIconProvider.kt
@@ -13,11 +13,35 @@ class DefaultFileIconProvider(context: Context) : FileIconProvider {
     private val folder = ContextCompat.getDrawable(context, R.drawable.folder)
     private val chevronRight = ContextCompat.getDrawable(context, R.drawable.ic_chevron_right)
     private val expandMore = ContextCompat.getDrawable(context, R.drawable.round_expand_more_24)
+    private val java = ContextCompat.getDrawable(context, R.drawable.ic_language_java)
+    private val html = ContextCompat.getDrawable(context, R.drawable.ic_language_html)
+    private val kotlin = ContextCompat.getDrawable(context, R.drawable.ic_language_kotlin)
+    private val python = ContextCompat.getDrawable(context, R.drawable.ic_language_python)
+    private val xml = ContextCompat.getDrawable(context, R.drawable.ic_language_xml)
+    private val js = ContextCompat.getDrawable(context, R.drawable.ic_language_js)
+    private val c = ContextCompat.getDrawable(context, R.drawable.ic_language_c)
+    private val cpp = ContextCompat.getDrawable(context, R.drawable.ic_language_cpp)
+    private val json = ContextCompat.getDrawable(context, R.drawable.ic_language_json)
+    private val css = ContextCompat.getDrawable(context, R.drawable.ic_language_css)
+    private val csharp = ContextCompat.getDrawable(context, R.drawable.ic_language_csharp)
 
     override fun getIcon(node: Node<FileObject>): Drawable? {
-        return if (node.value.isFile()){
-            file
-        }else{
+        return if (node.value.isFile()) {
+            when (node.value.getName().substringAfterLast('.', "")) {
+                "java", "bsh" -> java
+                "html" -> html
+                "kt", "kts" -> kotlin
+                "py" -> python
+                "xml" -> xml
+                "js" -> js
+                "c" -> c
+                "cpp", "h" -> cpp
+                "json" -> json
+                "css" -> css
+                "cs" -> csharp
+                else -> file
+            }
+        } else {
             folder
         }
     }
