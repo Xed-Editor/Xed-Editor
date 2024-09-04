5e9394d8364880e73e4df2c3515bd1dc6990a42b
RohitKushvaha01 <oldisg131@gmail.com>
2024-09-04 15:28:05 +0530

    support new proot

--- app/src/main/assets/proot.sh
+++ app/src/main/assets/proot.sh
@@ -8,6 +8,6 @@ else
     START_SHELL="/bin/sh"
 fi
 
-
+export LD_LIBRARY_PATH=$PREFIX_PATH/root/lib
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-$PREFIX_PATH/root/bin/proot -b /sdcard:/sdcard -b /storage:/storage -S $PREFIX_PATH/rootfs "$START_SHELL"
+$PREFIX_PATH/root/bin/proot -b /sdcard:/sdcard -b /storage:/storage -S $PREFIX_PATH/rootfs "$START_SHELL"i
