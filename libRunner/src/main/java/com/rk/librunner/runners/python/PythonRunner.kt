1f3834b018ae7c132910d863be9662c6630220e5
Andro_Dev <rudenko2007artem@gmail.com>
2024-09-24 08:27:46 +0300

    .

--- libRunner/src/main/java/com/rk/librunner/runners/python/PythonRunner.kt
+++ libRunner/src/main/java/com/rk/librunner/runners/python/PythonRunner.kt
@@ -11,7 +11,10 @@ class PythonRunner : RunnerImpl {
         runCommand(
             alpine = true,
             shell = "/bin/bash",
-            args = arrayOf("-c", "(apk info -e python3 || apk add python3) && (apk info -e py3-pip || apk add py3-pip) && python3 $file"),
+            args = arrayOf("-c", """
+            (apk info -e python3 || apk add python3) && 
+            (apk info -e py3-pip || apk add py3-pip) && 
+            python3 $file""".trimIndent()),
             workingDir = file.parentFile!!.absolutePath,
             context = context
         )
