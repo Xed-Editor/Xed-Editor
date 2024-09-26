62a59ba963a129f9d9655b8021f6d58ef515abdc
trindadedev13 <devsuay@gmail.com>
2024-09-26 09:14:51 -0300

    real

--- app/src/main/java/com/rk/xededitor/ui/theme/Type.kt
+++ app/src/main/java/com/rk/xededitor/ui/theme/Type.kt
@@ -2,33 +2,16 @@ package com.rk.xededitor.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontStyle
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-  bodyLarge = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
-    fontSize = 16.sp,
-    lineHeight = 24.sp,
-    letterSpacing = 0.5.sp
-  )
-  /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+import com.rk.xededitor.R
+
+val NunitoFontFamily = FontFamily(
+    Font(R.font.outfit_regular, FontWeight.Normal, FontStyle.Normal)
 )
+
+val Typography = Typography()
