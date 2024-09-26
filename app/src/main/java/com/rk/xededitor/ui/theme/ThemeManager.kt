4c920cf2ba341a4f96f39c25e91e9a95793a897b
trindadedev13 <devsuay@gmail.com>
2024-09-26 08:50:10 -0300

    fix:  more fixes

--- app/src/main/java/com/rk/xededitor/ui/theme/ThemeManager.kt
+++ app/src/main/java/com/rk/xededitor/ui/theme/ThemeManager.kt
@@ -1,7 +1,10 @@
 package com.rk.xededitor.ui.theme
 
+import android.app.Activity
 import android.content.Context
 import android.content.res.Resources
+
+import com.google.android.material.color.DynamicColors
 
 import com.rk.xededitor.R
 import com.rk.xededitor.Settings.SettingsData
@@ -13,12 +16,23 @@ import com.rk.xededitor.Settings.SettingsData
 
 object ThemeManager {
     
-    fun apply(context: Context) {
-        if (SettingsData.isDarkMode(context) && SettingsData.isOled()) {
-             context.setTheme(R.style.Theme_Karbon_Oled)
-             return
+    /*
+    * Function that applies the theme
+    * Checks if the user is in the dark theme, and if they have the oled theme enabled, if so, applies the oled theme.
+    * Checks if the user has the "Dynamic Colors" function activated, if so, applies Dynamic Colors.
+    * @param activity, An instance of an Activity.
+    */
+    fun apply(activity: Activity) {
+        if (SettingsData.isDarkMode(activity) && SettingsData.isOled()) {
+             activity.setTheme(R.style.Theme_Karbon_Oled)
+        }
+        if (SettingsData.isMonet()) {
+             DynamicColors.applyToActivityIfAvailable(activity)
         }
     }
     
-    fun getCurrentTheme(context: Context): Resources.Theme? = context.theme
+    /*
+    * @returns Return a current theme.
+    */
+    fun getCurrentTheme(ctx: Context): Resources.Theme? = ctx.theme
 }
