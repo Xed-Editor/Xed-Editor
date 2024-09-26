9767d83269407a23b8b1f46991216904bf6e4056
trindadedev13 <devsuay@gmail.com>
2024-09-26 08:13:53 -0300

    fix:  more fixes

--- app/src/main/java/com/rk/xededitor/Settings/SettingsApp.kt
+++ app/src/main/java/com/rk/xededitor/Settings/SettingsApp.kt
@@ -11,6 +11,7 @@ import android.view.WindowManager
 import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
