a4d1a8d66d61dd247c4a78341b328d42a0212b0a
RohitKushvaha01 <oldisg131@gmail.com>
2024-09-26 13:08:54 +0530

    extract

--- app/src/main/java/com/rk/xededitor/Settings/GitSettings.kt
+++ app/src/main/java/com/rk/xededitor/Settings/GitSettings.kt
@@ -19,7 +19,6 @@ import de.Maxr1998.modernpreferences.helpers.screen
 import de.Maxr1998.modernpreferences.helpers.pref
 import de.Maxr1998.modernpreferences.helpers.onClickView
 import java.io.File
-
 class GitSettings : BaseActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var binding: ActivitySettingsMainBinding
@@ -48,7 +47,7 @@ class GitSettings : BaseActivity() {
         }
 
         setContentView(binding.root)
-        binding.toolbar.title = "Git"
+        binding.toolbar.title = getString(R.string.git)
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         if (SettingsData.isDarkMode(this) && SettingsData.isOled()) {
@@ -69,18 +68,18 @@ class GitSettings : BaseActivity() {
     private fun getScreen(): PreferenceScreen {
         return screen(this) {
             pref(Keys.GIT_CREDENTIALS) {
-                title = "Credentials"
-                summary = "Credentials for git"
+                titleRes = R.string.cred
+                summaryRes = R.string.gitcred
                 iconRes = R.drawable.key
                 onClickView {
                     val view = LayoutInflater.from(this@GitSettings).inflate(R.layout.popup_new, null)
                     val edittext = view.findViewById<EditText>(R.id.name).apply {
-                        hint = "eg. SuperKek:ghp_..."
+                        hint = getString(R.string.gitKeyExample)
                         setText(SettingsData.getString(Keys.GIT_CRED, ""))
                     }
-                    MaterialAlertDialogBuilder(this@GitSettings).setTitle("Credentials")
-                        .setView(view).setNegativeButton("Cancel", null)
-                        .setPositiveButton("Apply") { _, _ ->
+                    MaterialAlertDialogBuilder(this@GitSettings).setTitle(getString(R.string.cred))
+                        .setView(view).setNegativeButton(getString(R.string.cancel), null)
+                        .setPositiveButton(getString(R.string.apply)) { _, _ ->
                             val credentials = edittext.text.toString()
                             if (credentials.isEmpty()) {
                                 return@setPositiveButton
@@ -90,18 +89,18 @@ class GitSettings : BaseActivity() {
                 }
             }
             pref(Keys.GIT_USER) {
-                title = "User data"
-                summary = "User data for git"
+                titleRes = R.string.userdata
+                summaryRes = R.string.userdatagit
                 iconRes = R.drawable.person
                 onClickView {
                     val view = LayoutInflater.from(this@GitSettings).inflate(R.layout.popup_new, null)
                     val edittext = view.findViewById<EditText>(R.id.name).apply {
-                        hint = "eg. UserDev:example@email.com"
+                        hint = getString(R.string.gituserexample)
                         setText(SettingsData.getString(Keys.GIT_USER_DATA, ""))
                     }
-                    MaterialAlertDialogBuilder(this@GitSettings).setTitle("User data")
-                        .setView(view).setNegativeButton("Cancel", null)
-                        .setPositiveButton("Apply") { _, _ ->
+                    MaterialAlertDialogBuilder(this@GitSettings).setTitle(getString(R.string.userdata))
+                        .setView(view).setNegativeButton(getString(R.string.cancel), null)
+                        .setPositiveButton(getString(R.string.apply)) { _, _ ->
                             val userdata = edittext.text.toString()
                             if (userdata.isEmpty()) {
                                 return@setPositiveButton
@@ -111,8 +110,8 @@ class GitSettings : BaseActivity() {
                 }
             }
             pref(Keys.GIT_DIR) {
-                title = "Repository folder"
-                summary = "Folder for cloning repositories"
+                titleRes = R.string.repo_dir
+                summaryRes = R.string.clone_dir
                 iconRes = R.drawable.outline_folder_24
                 onClickView {
                     val view = LayoutInflater.from(this@GitSettings).inflate(R.layout.popup_new, null)
@@ -120,15 +119,15 @@ class GitSettings : BaseActivity() {
                         hint = "/storage/emulated/0"
                         setText(SettingsData.getString(Keys.GIT_REPO_DIR, "/storage/emulated/0"))
                     }
-                    MaterialAlertDialogBuilder(this@GitSettings).setTitle("Repository folder")
-                        .setView(view).setNegativeButton("Cancel", null)
-                        .setPositiveButton("Apply") { _, _ ->
+                    MaterialAlertDialogBuilder(this@GitSettings).setTitle(getString(R.string.repo_dir))
+                        .setView(view).setNegativeButton(getString(R.string.cancel), null)
+                        .setPositiveButton(getString(R.string.apply)) { _, _ ->
                             val repodir = edittext.text.toString()
                             if (repodir.isEmpty()) {
                                 return@setPositiveButton
                             }
                             if (!File(repodir).exists()) {
-                                rkUtils.toast("Folder does not exists")
+                                rkUtils.toast(getString(R.string.dir_exist_not))
                                 return@setPositiveButton
                             }
                             SettingsData.setString(Keys.GIT_REPO_DIR, repodir)
