bf033bd409c0f3646bce36a731a4178e13de44a3
RohitKushvaha01 <oldisg131@gmail.com>
2024-09-30 11:34:18 +0530

    fix crash

--- app/src/main/java/com/rk/xededitor/ui/theme/ThemeManager.kt
+++ app/src/main/java/com/rk/xededitor/ui/theme/ThemeManager.kt
@@ -9,14 +9,14 @@ import com.google.android.material.color.DynamicColors
 import com.rk.xededitor.R
 import com.rk.xededitor.Settings.SettingsData
 
-/*
+/**
 * A Basic Helper for Apply correct theme in app
 * @author Aquiles Trindade (trindadedev).
 */
 
 object ThemeManager {
     
-    /*
+    /**
     * Function that applies the theme
     * Checks if the user is in the dark theme, and if they have the oled theme enabled, if so, applies the oled theme.
     * Checks if the user has the "Dynamic Colors" function activated, if so, applies Dynamic Colors.
@@ -31,7 +31,7 @@ object ThemeManager {
         }
     }
     
-    /*
+    /**
     * @returns Return a current theme.
     */
     fun getCurrentTheme(ctx: Context): Resources.Theme? = ctx.theme
