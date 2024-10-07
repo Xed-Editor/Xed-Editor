faf3e2c29aa5d5808965dae499ed7ee700b867ae
trindadedev13 <devsuay@gmail.com>
2024-10-07 04:39:50 -0300

    fix: ui.unit.draw package not exits

--- app/src/main/java/com/rk/xededitor/ui/screens/settings/manage/ManagePluginsScreen.kt
+++ app/src/main/java/com/rk/xededitor/ui/screens/settings/manage/ManagePluginsScreen.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.draw.clip
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewModelScope
