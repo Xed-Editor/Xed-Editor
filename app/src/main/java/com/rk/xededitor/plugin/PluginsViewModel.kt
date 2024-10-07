46e825c7857ff831ed845b139e852bb7fb5465aa
trindadedev13 <devsuay@gmail.com>
2024-10-07 10:59:37 -0300

    feat: Organize Code & Cleanup

--- app/src/main/java/com/rk/xededitor/plugin/PluginsViewModel.kt
+++ app/src/main/java/com/rk/xededitor/plugin/PluginsViewModel.kt
@@ -15,7 +15,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class PluginModel : ViewModel() {
+class PluginViewModel : ViewModel() {
     var plugins by mutableStateOf(listOf<Plugin>())
     var availablePlugins = listOf<PluginItem>()
     var isLoading by mutableStateOf(false)
