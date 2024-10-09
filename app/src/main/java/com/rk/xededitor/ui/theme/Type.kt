0aed2d28b0fd79bce34ba5ed1979a8186f574eb4
trindadedev13 <devsuay@gmail.com>
2024-10-09 01:12:48 -0300

    feat: Apply font on theme

--- app/src/main/java/com/rk/xededitor/ui/theme/Type.kt
+++ app/src/main/java/com/rk/xededitor/ui/theme/Type.kt
@@ -1,14 +1,27 @@
 package com.rk.xededitor.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.rk.xededitor.R
 
-val NunitoFontFamily = FontFamily(
+val OutfitFontFamily = FontFamily(
     Font(R.font.outfit_regular, FontWeight.Normal, FontStyle.Normal)
 )
 
-val Typography = Typography()
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = OutfitFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = OutfitFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    )
+)
