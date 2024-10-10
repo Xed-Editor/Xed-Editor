42f6c4ec49819a7c81b9b659e95c2696b4b7f5c6
trindadedev13 <devsuay@gmail.com>
2024-10-10 15:16:24 -0300

    feat: Decrease SlideDurationMillis to 300

--- app/src/main/java/com/rk/xededitor/ui/animations/NavigationAnimationValues.kt
+++ app/src/main/java/com/rk/xededitor/ui/animations/NavigationAnimationValues.kt
@@ -2,5 +2,5 @@ package com.rk.xededitor.ui.animations
 
 object NavigationAnimationValues {
     const val SlideDistance: Int = 30
-    const val SlideDuration: Int = 700
+    const val SlideDuration: Int = 300
 }
