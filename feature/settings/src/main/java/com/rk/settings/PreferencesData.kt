02ac4012b5086aa1418ee5af33435a36898c5292
trindadedev13 <devsuay@gmail.com>
2024-10-10 16:07:32 -0300

    fixes

--- feature/settings/src/main/java/com/rk/settings/PreferencesData.kt
+++ feature/settings/src/main/java/com/rk/settings/PreferencesData.kt
@@ -8,8 +8,8 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "Settings")
 
@@ -20,36 +20,41 @@ object PreferencesData {
     private val OLED_KEY = booleanPreferencesKey(PreferencesKeys.OLED)
     private val MONET_KEY = booleanPreferencesKey(PreferencesKeys.MONET)
 
-    fun isDarkMode(ctx: Context): Boolean {
-        val preferences = ctx.dataStore.data.map { it[DEFAULT_NIGHT_MODE_KEY] ?: AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM.toString() }
-        val mode = preferences.first()
-
-        return when (mode) {
-            AppCompatDelegate.MODE_NIGHT_YES.toString() -> true
-            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM.toString() -> {
-                ((ctx.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) ==
-                        Configuration.UI_MODE_NIGHT_YES)
+    fun isDarkMode(ctx: Context): LiveData<Boolean> {
+        val preferencesFlow = ctx.dataStore.data
+            .map { preferences ->
+                val mode = preferences[DEFAULT_NIGHT_MODE_KEY] ?: AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM.toString()
+                when (mode) {
+                    AppCompatDelegate.MODE_NIGHT_YES.toString() -> true
+                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM.toString() -> {
+                        (ctx.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) ==
+                        Configuration.UI_MODE_NIGHT_YES
+                    }
+                    else -> false
+                }
             }
-            else -> false
-        }
+        return preferencesFlow.asLiveData()
     }
 
-    fun isOled(): Boolean {
-        val preferences = ctx.dataStore.data.map { it[OLED_KEY] ?: false }
-        return preferences.first()
+    fun isOled(ctx: Context): LiveData<Boolean> {
+        val preferencesFlow = ctx.dataStore.data
+            .map { preferences -> preferences[OLED_KEY] ?: false }
+        return preferencesFlow.asLiveData()
     }
 
-    fun isMonet(): Boolean {
-        val preferences = ctx.dataStore.data.map { it[MONET_KEY] ?: false }
-        return preferences.first() 
+    fun isMonet(ctx: Context): LiveData<Boolean> {
+        val preferencesFlow = ctx.dataStore.data
+            .map { preferences -> preferences[MONET_KEY] ?: false }
+        return preferencesFlow.asLiveData()
     }
 
-    fun getBoolean(key: String?, default: Boolean): Boolean {
-        val preferences = ctx.dataStore.data.map { it[booleanPreferencesKey(key)] ?: default }
-        return preferences.first() 
+    fun getBoolean(ctx: Context, key: String?, default: Boolean): LiveData<Boolean> {
+        val preferencesFlow = ctx.dataStore.data
+            .map { preferences -> preferences[booleanPreferencesKey(key)] ?: default }
+        return preferencesFlow.asLiveData()
     }
 
-    fun setBoolean(key: String?, value: Boolean) {
+    fun setBoolean(ctx: Context, key: String?, value: Boolean) {
         ctx.dataStore.edit { preferences ->
             preferences[booleanPreferencesKey(key)] = value
         }
@@ -57,12 +62,13 @@ object PreferencesData {
 
     fun initPref(ctx: Context) { }
 
-    fun getString(key: String?, default: String): String {
-        val preferences = ctx.dataStore.data.map { it[stringPreferencesKey(key)] ?: default }
-        return preferences.first()
+    fun getString(ctx: Context, key: String?, default: String): LiveData<String> {
+        val preferencesFlow = ctx.dataStore.data
+            .map { preferences -> preferences[stringPreferencesKey(key)] ?: default }
+        return preferencesFlow.asLiveData()
     }
 
-    fun setString(key: String?, value: String?) {
+    fun setString(ctx: Context, key: String?, value: String?) {
         ctx.dataStore.edit { preferences ->
             preferences[stringPreferencesKey(key)] = value
         }
