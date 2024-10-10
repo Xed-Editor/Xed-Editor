bc35642db63935534a7f0fc0739ebee8962145d4
trindadedev13 <devsuay@gmail.com>
2024-10-10 14:50:47 -0300

    fix: decrease default SlideDistance to 30

--- app/src/main/java/com/rk/xededitor/ui/animations/NavigationAnimationValues.kt
+++ app/src/main/java/com/rk/xededitor/ui/animations/NavigationAnimationValues.kt
@@ -1,6 +1,6 @@
 package com.rk.xededitor.ui.animations
 
 object NavigationAnimationValues {
-    const val SlideDistance: Int = 100
+    const val SlideDistance: Int = 30
     const val SlideDuration: Int = 700
 }
