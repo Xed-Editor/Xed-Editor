dd16f062fb89e0460153ab7a652d86e88cb08180
RohitKushvaha01 <oldisg131@gmail.com>
2024-10-15 17:20:08 +0530

    fix : apt not found

--- app/src/main/assets/proot.sh
+++ app/src/main/assets/proot.sh
@@ -1,5 +1,5 @@
 PREFIX_PATH=/data/data/com.rk.xededitor
 export LD_LIBRARY_PATH=$PREFIX_PATH/root/lib
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+PATH=/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 ARGS="$(cat $PREFIX_PATH/proot_args)"
 $PREFIX_PATH/root/bin/proot -b /sdcard:/sdcard -b /storage:/storage -b $PREFIX_PATH:$PREFIX_PATH -b $PREFIX_PATH:/karbon $ARGS -S $PREFIX_PATH/rootfs sh /init.sh "$@"
