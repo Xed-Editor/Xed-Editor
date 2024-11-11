952029d9705401f1d50a06f9910a9f42bcc3de79
RohitKushvaha01 <oldisg131@gmail.com>
2024-11-11 09:21:17 +0530

    feat. wasm3

--- app/src/main/wasm/setup.sh
+++
@@ -1,5 +0,0 @@
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
-      -DANDROID_ABI=arm64-v8a \
-      -DANDROID_PLATFORM=android-21 \
-      -DCMAKE_BUILD_TYPE=Release \
-      . 
