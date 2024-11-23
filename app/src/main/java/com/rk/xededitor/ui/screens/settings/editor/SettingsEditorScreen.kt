6bea1b591964598ceeedc907bfd2808f55a8dab2
RohitKushvaha01 <oldisg131@gmail.com>
2024-11-23 15:09:44 +0530

    feat. categorized editor settings

--- app/src/main/java/com/rk/xededitor/ui/screens/settings/editor/SettingsEditorScreen.kt
+++ app/src/main/java/com/rk/xededitor/ui/screens/settings/editor/SettingsEditorScreen.kt
@@ -15,9 +15,9 @@ import com.rk.xededitor.MainActivity.tabs.editor.AutoSaver
 import com.rk.xededitor.MainActivity.tabs.editor.EditorFragment
 import com.rk.xededitor.R
 import com.rk.xededitor.rkUtils
-import com.rk.xededitor.rkUtils.getString
 import com.rk.xededitor.ui.components.InputDialog
 import com.rk.xededitor.ui.components.SettingsToggle
+import org.robok.engine.core.components.compose.preferences.base.PreferenceGroup
 import org.robok.engine.core.components.compose.preferences.base.PreferenceLayout
 import org.robok.engine.core.components.compose.preferences.category.PreferenceCategory
 
@@ -25,194 +25,6 @@ import org.robok.engine.core.components.compose.preferences.category.PreferenceC
 fun SettingsEditorScreen() {
     PreferenceLayout(label = stringResource(id = R.string.editor), backArrowVisible = true) {
         val context = LocalContext.current
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.smooth_tabs),
-            description = stringResource(id = R.string.smooth_tab_desc),
-            iconRes = R.drawable.animation,
-            key = PreferencesKeys.VIEWPAGER_SMOOTH_SCROLL,
-            default = false,
-            sideEffect = {
-                smoothTabs = it
-            }
-        )
-        
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.ww),
-            description = stringResource(id = R.string.ww_desc),
-            iconRes = R.drawable.reorder,
-            key = PreferencesKeys.WORD_WRAP_ENABLED,
-            default = false,
-            sideEffect = {
-                MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
-                    if (f.value.get()?.fragment is EditorFragment) {
-                        (f.value.get()?.fragment as EditorFragment).editor?.isWordwrap = it
-                    }
-                }
-            }
-        )
-        
-        SettingsToggle(
-            label = "Anti word breaking",
-            description = "don't break words in word wrap",
-            iconRes = R.drawable.reorder,
-            key = PreferencesKeys.ANTI_WORD_BREAKING,
-            default = true,
-            sideEffect = {
-                MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
-                    if (f.value.get()?.fragment is EditorFragment) {
-                        (f.value.get()?.fragment as EditorFragment).editor?.setWordwrap(PreferencesData.getBoolean(PreferencesKeys.WORD_WRAP_ENABLED,false),it)
-                    }
-                }
-            }
-        )
-        
-        SettingsToggle(
-            label = stringResource(R.string.txt_ww),
-            description = stringResource(R.string.txt_ww_desc),
-            iconRes = R.drawable.reorder,
-            key = PreferencesKeys.WORD_WRAP_TXT,
-            default = false,
-            sideEffect = {
-                MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
-                    if (f.value.get()?.fragment is EditorFragment) {
-                        (f.value.get()?.fragment as EditorFragment).apply {
-                            if (file?.name?.endsWith(".txt") == true) {
-                                if (editor?.isWordwrap!!.not()){
-                                    editor?.isWordwrap = it
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        )
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.keepdl),
-            description = stringResource(id = R.string.drawer_lock_desc),
-            iconRes = R.drawable.lock,
-            key = PreferencesKeys.KEEP_DRAWER_LOCKED,
-            default = false,
-        )
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.diagonal_scroll),
-            description = stringResource(id = R.string.diagonal_scroll_desc),
-            iconRes = R.drawable.diagonal_scroll,
-            key = PreferencesKeys.DIAGONAL_SCROLL,
-            default = true,
-            sideEffect = {
-                rkUtils.toast(getString(R.string.rr))
-            }
-        )
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.cursor_anim),
-            description = stringResource(id = R.string.cursor_anim_desc),
-            iconRes = R.drawable.animation,
-            key = PreferencesKeys.CURSOR_ANIMATION_ENABLED,
-            default = false,
-            sideEffect = {
-                MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
-                    if (f.value.get()?.fragment is EditorFragment) {
-                        (f.value.get()?.fragment as EditorFragment).editor?.isCursorAnimationEnabled = it
-                    }
-                }
-            }
-        )
-        
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.show_line_number),
-            description = stringResource(id = R.string.show_line_number),
-            iconRes = R.drawable.linenumbers,
-            key = PreferencesKeys.SHOW_LINE_NUMBERS,
-            default = true,
-            sideEffect = {
-                MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
-                    if (f.value.get()?.fragment is EditorFragment) {
-                        (f.value.get()?.fragment as EditorFragment).editor?.isLineNumberEnabled = it
-                    }
-                }
-            }
-        )
-        
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.pin_line_number),
-            description = stringResource(id = R.string.pin_line_number),
-            iconRes = R.drawable.linenumbers,
-            key = PreferencesKeys.PIN_LINE_NUMBER,
-            default = false,
-            sideEffect = {
-                MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
-                    if (f.value.get()?.fragment is EditorFragment) {
-                        (f.value.get()?.fragment as EditorFragment).editor?.setPinLineNumber(it)
-                    }
-                }
-            }
-        )
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.show_suggestions),
-            description = stringResource(id = R.string.show_suggestions),
-            iconRes = R.drawable.baseline_font_download_24,
-            key = PreferencesKeys.SHOW_SUGGESTIONS,
-            default = false,
-            sideEffect = {
-                MainActivity.activityRef.get()?.adapter!!.tabFragments.values.forEach { f ->
-                    if (f.get()?.fragment is EditorFragment) {
-                        (f.get()?.fragment as EditorFragment).editor?.showSuggestions(it)
-                    }
-                }
-            }
-        )
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.extra_keys),
-            description = stringResource(id = R.string.extra_keys_desc),
-            iconRes = R.drawable.double_arrows,
-            key = PreferencesKeys.SHOW_ARROW_KEYS,
-            default = true,
-            sideEffect = {
-                MainActivity.activityRef.get()?.let { activity ->
-                    if (activity.tabViewModel.fragmentFiles.isEmpty()) {
-                        return@let
-                    }
-                    
-                    MainActivity.activityRef.get()?.adapter?.tabFragments?.values?.forEach { f ->
-                        if (f.get()?.fragment is EditorFragment) {
-                            (f.get()?.fragment as EditorFragment).showArrowKeys(it)
-                        }
-                    }
-                }
-            }
-        )
-        
-        
-        
-        
-        
-        SettingsToggle(
-            label = stringResource(id = R.string.auto_save),
-            description = stringResource(id = R.string.auto_save_desc),
-            iconRes = R.drawable.save,
-            key = PreferencesKeys.AUTO_SAVE,
-            default = false,
-        )
-        
-        SettingsToggle(
-            label = stringResource(R.string.sora_s),
-            description = stringResource(R.string.sora_s_desc),
-            iconRes = R.drawable.search,
-            key = PreferencesKeys.USE_SORA_SEARCH,
-            default = false,
-        )
-        
-        
-        
         
         var showAutoSaveDialog by remember { mutableStateOf(false) }
         var showTextSizeDialog by remember { mutableStateOf(false) }
@@ -227,24 +39,188 @@ fun SettingsEditorScreen() {
             mutableStateOf(PreferencesData.getString(PreferencesKeys.TAB_SIZE, "4"))
         }
         
-        PreferenceCategory(
-            label = stringResource(id = R.string.auto_save_time),
-            description = stringResource(id = R.string.auto_save_time_desc),
-            iconResource = R.drawable.save,
-            onNavigate = { showAutoSaveDialog = true },
-        )
-        PreferenceCategory(
-            label = stringResource(id = R.string.text_size),
-            description = stringResource(id = R.string.text_size_desc),
-            iconResource = R.drawable.reorder,
-            onNavigate = { showTextSizeDialog = true },
-        )
-        PreferenceCategory(
-            label = stringResource(id = R.string.tab_size),
-            description = stringResource(id = R.string.tab_size_desc),
-            iconResource = R.drawable.double_arrows,
-            onNavigate = { showTabSizeDialog = true },
-        )
+
+        
+        PreferenceGroup(heading = "content") {
+            SettingsToggle(label = stringResource(id = strings.ww),
+                description = stringResource(id = strings.ww_desc),
+                iconRes = drawable.reorder,
+                key = PreferencesKeys.WORD_WRAP_ENABLED,
+                default = false,
+                sideEffect = {
+                    MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
+                        if (f.value.get()?.fragment is EditorFragment) {
+                            (f.value.get()?.fragment as EditorFragment).editor?.isWordwrap = it
+                        }
+                    }
+                })
+            SettingsToggle(label = stringResource(strings.txt_ww),
+                description = stringResource(strings.txt_ww_desc),
+                iconRes = drawable.reorder,
+                key = PreferencesKeys.WORD_WRAP_TXT,
+                default = false,
+                sideEffect = {
+                    MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
+                        if (f.value.get()?.fragment is EditorFragment) {
+                            (f.value.get()?.fragment as EditorFragment).apply {
+                                if (file?.name?.endsWith(".txt") == true) {
+                                    if (editor?.isWordwrap!!.not()) {
+                                        editor?.isWordwrap = it
+                                    }
+                                }
+                            }
+                        }
+                    }
+                })
+            SettingsToggle(label = "Anti word breaking",
+                description = "don't break words in word wrap",
+                iconRes = drawable.reorder,
+                key = PreferencesKeys.ANTI_WORD_BREAKING,
+                default = true,
+                sideEffect = {
+                    MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
+                        if (f.value.get()?.fragment is EditorFragment) {
+                            (f.value.get()?.fragment as EditorFragment).editor?.setWordwrap(
+                                PreferencesData.getBoolean(
+                                    PreferencesKeys.WORD_WRAP_ENABLED, false
+                                ), it
+                            )
+                        }
+                    }
+                })
+        }
+        
+        
+        
+        PreferenceGroup(heading = "editor") {
+            SettingsToggle(label = stringResource(id = strings.cursor_anim),
+                description = stringResource(id = strings.cursor_anim_desc),
+                iconRes = drawable.animation,
+                key = PreferencesKeys.CURSOR_ANIMATION_ENABLED,
+                default = false,
+                sideEffect = {
+                    MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
+                        if (f.value.get()?.fragment is EditorFragment) {
+                            (f.value.get()?.fragment as EditorFragment).editor?.isCursorAnimationEnabled = it
+                        }
+                    }
+                })
+            SettingsToggle(label = stringResource(id = strings.show_line_number),
+                description = stringResource(id = strings.show_line_number),
+                iconRes = drawable.linenumbers,
+                key = PreferencesKeys.SHOW_LINE_NUMBERS,
+                default = true,
+                sideEffect = {
+                    MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
+                        if (f.value.get()?.fragment is EditorFragment) {
+                            (f.value.get()?.fragment as EditorFragment).editor?.isLineNumberEnabled = it
+                        }
+                    }
+                })
+            SettingsToggle(label = stringResource(id = strings.show_suggestions),
+                description = stringResource(id = strings.show_suggestions),
+                iconRes = drawable.baseline_font_download_24,
+                key = PreferencesKeys.SHOW_SUGGESTIONS,
+                default = false,
+                sideEffect = {
+                    MainActivity.activityRef.get()?.adapter!!.tabFragments.values.forEach { f ->
+                        if (f.get()?.fragment is EditorFragment) {
+                            (f.get()?.fragment as EditorFragment).editor?.showSuggestions(it)
+                        }
+                    }
+                })
+            SettingsToggle(label = stringResource(id = strings.pin_line_number),
+                description = stringResource(id = strings.pin_line_number),
+                iconRes = drawable.linenumbers,
+                key = PreferencesKeys.PIN_LINE_NUMBER,
+                default = false,
+                sideEffect = {
+                    MainActivity.activityRef.get()?.adapter?.tabFragments?.forEach { f ->
+                        if (f.value.get()?.fragment is EditorFragment) {
+                            (f.value.get()?.fragment as EditorFragment).editor?.setPinLineNumber(it)
+                        }
+                    }
+                })
+            SettingsToggle(
+                label = stringResource(id = strings.editor_font),
+                description = stringResource(id = strings.editor_font_desc),
+                iconRes = drawable.baseline_font_download_24,
+                key = PreferencesKeys.EDITOR_FONT,
+                default = false,
+            )
+            PreferenceCategory(
+                label = stringResource(id = strings.text_size),
+                description = stringResource(id = strings.text_size_desc),
+                iconResource = drawable.reorder,
+                onNavigate = { showTextSizeDialog = true },
+            )
+        }
+        
+        
+        
+        PreferenceGroup(heading = "misc.") {
+            SettingsToggle(label = stringResource(id = strings.extra_keys),
+                description = stringResource(id = strings.extra_keys_desc),
+                iconRes = drawable.double_arrows,
+                key = PreferencesKeys.SHOW_ARROW_KEYS,
+                default = true,
+                sideEffect = {
+                    MainActivity.activityRef.get()?.let { activity ->
+                        if (activity.tabViewModel.fragmentFiles.isEmpty()) {
+                            return@let
+                        }
+                        
+                        MainActivity.activityRef.get()?.adapter?.tabFragments?.values?.forEach { f ->
+                            if (f.get()?.fragment is EditorFragment) {
+                                (f.get()?.fragment as EditorFragment).showArrowKeys(it)
+                            }
+                        }
+                    }
+                })
+            SettingsToggle(label = stringResource(id = strings.smooth_tabs),
+                description = stringResource(id = strings.smooth_tab_desc),
+                iconRes = drawable.animation,
+                key = PreferencesKeys.VIEWPAGER_SMOOTH_SCROLL,
+                default = false,
+                sideEffect = {
+                    smoothTabs = it
+                })
+            SettingsToggle(
+                label = stringResource(id = strings.keepdl),
+                description = stringResource(id = strings.drawer_lock_desc),
+                iconRes = drawable.lock,
+                key = PreferencesKeys.KEEP_DRAWER_LOCKED,
+                default = false,
+            )
+            SettingsToggle(
+                label = stringResource(id = strings.auto_save),
+                description = stringResource(id = strings.auto_save_desc),
+                iconRes = drawable.save,
+                key = PreferencesKeys.AUTO_SAVE,
+                default = false,
+            )
+            
+            SettingsToggle(
+                label = stringResource(strings.sora_s),
+                description = stringResource(strings.sora_s_desc),
+                iconRes = drawable.search,
+                key = PreferencesKeys.USE_SORA_SEARCH,
+                default = false,
+            )
+            PreferenceCategory(
+                label = stringResource(id = strings.auto_save_time),
+                description = stringResource(id = strings.auto_save_time_desc),
+                iconResource = drawable.save,
+                onNavigate = { showAutoSaveDialog = true },
+            )
+            
+            PreferenceCategory(
+                label = stringResource(id = strings.tab_size),
+                description = stringResource(id = strings.tab_size_desc),
+                iconResource = drawable.double_arrows,
+                onNavigate = { showTabSizeDialog = true },
+            )
+        }
         
         if (showAutoSaveDialog) {
             InputDialog(
@@ -323,12 +299,5 @@ fun SettingsEditorScreen() {
             )
         }
         
-        SettingsToggle(
-            label = stringResource(id = R.string.editor_font),
-            description = stringResource(id = R.string.editor_font_desc),
-            iconRes = R.drawable.baseline_font_download_24,
-            key = PreferencesKeys.EDITOR_FONT,
-            default = false,
-        )
     }
 }
