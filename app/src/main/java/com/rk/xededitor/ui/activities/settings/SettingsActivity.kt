bdad12204e95f0eff25c663801ae464ea0d7ff50
RohitKushvaha01 <oldisg131@gmail.com>
2024-12-06 10:10:10 +0530

    fix. white flicker in settings

--- app/src/main/java/com/rk/xededitor/ui/activities/settings/SettingsActivity.kt
+++ app/src/main/java/com/rk/xededitor/ui/activities/settings/SettingsActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.material3.Surface
 import androidx.navigation.compose.rememberNavController
-import com.rk.xededitor.BaseActivity
 import com.rk.xededitor.ui.theme.KarbonTheme
 
 class SettingsActivity : ComponentActivity() {
@@ -16,11 +14,12 @@ class SettingsActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val backgroundColor = MaterialTheme.colorScheme.background.toArgb()
-            window.decorView.setBackgroundColor(backgroundColor)
             KarbonTheme {
-                val navController = rememberNavController()
-                SettingsNavHost(activity = this@SettingsActivity, navController = navController)
+                Surface {
+                    val navController = rememberNavController()
+                    SettingsNavHost(activity = this@SettingsActivity, navController = navController)
+
+                }
             }
         }
     }
