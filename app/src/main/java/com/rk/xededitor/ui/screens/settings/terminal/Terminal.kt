d6feede0199d180db05932b7edd622f86dcc3f73
Rohit Kushvaha <oldisg131@gmail.com>
2024-12-14 10:02:03 +0530

    feat. terminal adjust resize

--- app/src/main/java/com/rk/xededitor/ui/screens/settings/terminal/Terminal.kt
+++ app/src/main/java/com/rk/xededitor/ui/screens/settings/terminal/Terminal.kt
@@ -4,9 +4,11 @@ import androidx.compose.material3.Text
 import android.app.Activity
 import android.util.TypedValue
 import android.view.View
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -40,10 +42,10 @@ var virtualKeysId = View.generateViewId()
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Terminal(modifier: Modifier = Modifier) {
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    Box(modifier = Modifier.imePadding()) {
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
-    val scope = rememberCoroutineScope()
-
+        val scope = rememberCoroutineScope()
 
     ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
         ModalDrawerSheet {
@@ -55,26 +57,65 @@ fun Terminal(modifier: Modifier = Modifier) {
                 IconButton(onClick = {
                     scope.launch {
                         drawerState.open()
-                    }
-                }) {
-                    Icon(Icons.Default.Menu, "Menu")
-                }
-            })
-        }) { paddingValues ->
-            Column(modifier = Modifier.padding(paddingValues)) {
-                // TerminalView takes available space
-                val activity = LocalContext.current as? Activity
-                AndroidView(
-                    factory = { context ->
-                        TerminalView(context, null).apply {
-                            terminalView = WeakReference(this)
-                            setTextSize(rkUtils.dpToPx(14f, context))
-                            val client = TerminalBackEnd(this, activity!!)
-                            setTerminalViewClient(client)
-                            val session = MkSession.createSession(activity, client)
-                            attachSession(session)
 
-                            post {
+                        }
+                    }) {
+                        Icon(Icons.Default.Menu, "Menu")
+                    }
+                })
+            }) { paddingValues ->
+
+                Column(modifier = Modifier.padding(paddingValues)) {
+                    // TerminalView takes available space
+                    val activity = LocalContext.current as? Activity
+                    AndroidView(
+                        factory = { context ->
+                            TerminalView(context, null).apply {
+                                terminalView = WeakReference(this)
+                                setTextSize(rkUtils.dpToPx(14f, context))
+                                val client = TerminalBackEnd(this, activity!!)
+                                setTerminalViewClient(client)
+                                val session = MkSession.createSession(activity, client)
+                                attachSession(session)
+
+                                post {
+                                    val typedValue = TypedValue()
+                                    context.theme.resolveAttribute(
+                                        com.google.android.material.R.attr.colorSurface,
+                                        typedValue,
+                                        true
+                                    )
+                                    val surfaceColor = typedValue.data
+
+                                    context.theme.resolveAttribute(
+                                        com.google.android.material.R.attr.colorOnSurface,
+                                        typedValue,
+                                        true
+                                    )
+
+                                    setBackgroundColor(surfaceColor)
+                                    keepScreenOn = true
+                                    requestFocus()
+                                    setFocusableInTouchMode(true)
+
+                                    mEmulator?.mColors?.mCurrentColors?.apply {
+                                        set(256, typedValue.data)
+                                        set(258, typedValue.data)
+                                    }
+                                }
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
+                        update = { terminalView -> terminalView.onScreenUpdated() },
+                    )
+
+                    AndroidView(
+                        factory = { context ->
+                            VirtualKeysView(context, null).apply {
+
+                                id = virtualKeysId
                                 val typedValue = TypedValue()
                                 context.theme.resolveAttribute(
                                     com.google.android.material.R.attr.colorSurface,
@@ -89,61 +130,29 @@ fun Terminal(modifier: Modifier = Modifier) {
                                     true
                                 )
 
+
+                                virtualKeysViewClient =
+                                    terminalView.get()?.mTermSession?.let { VirtualKeysListener(it) }
+
+                                buttonTextColor = typedValue.data
                                 setBackgroundColor(surfaceColor)
-                                keepScreenOn = true
-                                requestFocus()
-                                setFocusableInTouchMode(true)
 
-                                mEmulator?.mColors?.mCurrentColors?.apply {
-                                    set(256, typedValue.data)
-                                    set(258, typedValue.data)
-                                }
-                            }
-                        }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
-                    update = { terminalView -> terminalView.onScreenUpdated() },
-                )
-
-                AndroidView(
-                    factory = { context ->
-                        VirtualKeysView(context, null).apply {
-
-                            id = virtualKeysId
-                            val typedValue = TypedValue()
-                            context.theme.resolveAttribute(
-                                com.google.android.material.R.attr.colorSurface, typedValue, true
-                            )
-                            val surfaceColor = typedValue.data
-
-                            context.theme.resolveAttribute(
-                                com.google.android.material.R.attr.colorOnSurface, typedValue, true
-                            )
-
-
-                            virtualKeysViewClient =
-                                terminalView.get()?.mTermSession?.let { VirtualKeysListener(it) }
-
-                            buttonTextColor = typedValue.data
-                            setBackgroundColor(surfaceColor)
-
-                            reload(
-                                VirtualKeysInfo(
-                                    VIRTUAL_KEYS, "", VirtualKeysConstants.CONTROL_CHARS_ALIASES
+                                reload(
+                                    VirtualKeysInfo(
+                                        VIRTUAL_KEYS, "", VirtualKeysConstants.CONTROL_CHARS_ALIASES
+                                    )
                                 )
-                            )
-                        }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(75.dp)
-                )
-            }
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(75.dp)
+                    )
+                }
 
-        }
-    })
+            }
+        })
+    }
 }
 
 const val VIRTUAL_KEYS =
