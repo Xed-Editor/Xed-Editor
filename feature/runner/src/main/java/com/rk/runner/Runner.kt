d5b234ecd61e10ef5d49696d0ef885b4a53e06ad
Rohit Kushvaha <oldisg131@gmail.com>
2024-12-20 17:08:30 +0530

    feat. enable web runners

--- feature/runner/src/main/java/com/rk/runner/Runner.kt
+++ feature/runner/src/main/java/com/rk/runner/Runner.kt
@@ -44,8 +44,8 @@ object Runner {
     val registry = HashMap<String, MutableList<RunnerImpl>>()
 
     init {
-//        registry["html"] = mutableListOf(HtmlRunner())
-//        registry["md"] = mutableListOf(MarkDownRunner())
+      registry["html"] = mutableListOf(HtmlRunner())
+      registry["md"] = mutableListOf(MarkDownRunner())
 //        registry["py"] = mutableListOf(PythonRunner())
 ////        registry["java"] =
 ////            mutableListOf(JavaRunner("Java"), JavaRunner("Javac"), JavaRunner("Maven"))
