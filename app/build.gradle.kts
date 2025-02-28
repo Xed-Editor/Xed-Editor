f9400a15918faaf0e7f5b9ee1e9ee78481ea0c99
RohitKushvaha01 <oldisg131@gmail.com>
2025-02-28 15:45:30 +0530

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,8 @@ android {
         targetSdk = 28
 
         //versioning
-        versionCode = 45
-        versionName = "2.9.9"
+        versionCode = 44
+        versionName = "2.9.8"
         vectorDrawables {
             useSupportLibrary = true
         }
