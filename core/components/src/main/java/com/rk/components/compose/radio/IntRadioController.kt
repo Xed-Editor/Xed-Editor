c62ba56104de08643bfe8d250e1646ccdccd18f4
RohitKushvaha01 <oldisg131@gmail.com>
2025-02-28 13:09:51 +0530

    remove gpl statements from apache license

--- core/components/src/main/java/com/rk/components/compose/radio/IntRadioController.kt
+++ core/components/src/main/java/com/rk/components/compose/radio/IntRadioController.kt
@@ -1,21 +1,5 @@
 package com.rk.components.compose.radio
 
-/*
- *  This file is part of Robok © 2024.
- *
- *  Robok is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Robok is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with Robok.  If not, see <https://www.gnu.org/licenses/>.
- */
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
