ce5c844d9845a42a6f295b5e6292348414ca20e1
Rohit Kushvaha <oldisg131@gmail.com>
2025-04-04 08:03:47 +0530

    Update init.sh

--- core/main/src/main/assets/terminal/init.sh
+++ core/main/src/main/assets/terminal/init.sh
@@ -6,7 +6,7 @@ export HOME=/root
 export PS1="\[\e[38;5;46m\]\u\[\033[39m\]@karbon \[\033[39m\]\w \[\033[0m\]\\$ "
 START_SHELL="/bin/bash"
 # shellcheck disable=SC2034
-PIP_BREAK_SYSTEM_PACKAGES=1
+export PIP_BREAK_SYSTEM_PACKAGES=1
 required_packages="bash gcompat glib git nano sudo file build-base"
 missing_packages=""
 for pkg in $required_packages; do
