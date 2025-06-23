cad22af72a0d68703706bb9fa389e4bd3888175b
RohitKushvaha01 <oldisg131@gmail.com>
2025-06-23 14:32:59 +0530

    fix. F-Droid reproducible build (Closes #637)

--- core/terminal-emulator/src/main/jni/Android.mk
+++ core/terminal-emulator/src/main/jni/Android.mk
@@ -2,4 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE:= libtermux
 LOCAL_SRC_FILES:= termux.c
+
+LOCAL_CFLAGS += -ffile-prefix-map=$(LOCAL_PATH)=.
+LOCAL_LDFLAGS += -Wl,--build-id=none
+
 include $(BUILD_SHARED_LIBRARY)
