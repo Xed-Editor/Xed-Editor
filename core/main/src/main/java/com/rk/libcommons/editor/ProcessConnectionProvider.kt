1bd1d4b7fb0f1c4963d426ed9e3c68c90b3bd3b3
RohitKushvaha01 <oldisg131@gmail.com>
2025-08-02 19:20:09 +0530

    process connection provider

---
+++ core/main/src/main/java/com/rk/libcommons/editor/ProcessConnectionProvider.kt
@@ -0,0 +1,46 @@
+package com.rk.libcommons.editor
+
+import com.rk.libcommons.child
+import com.rk.libcommons.localBinDir
+import io.github.rosemoe.sora.lsp.client.connection.StreamConnectionProvider
+import java.io.InputStream
+import java.io.OutputStream
+
+class ProcessConnectionProvider(
+    private val command: Array<String>,
+    private val alpine: Boolean = true
+) : StreamConnectionProvider {
+
+    private var process: Process? = null
+
+    override val inputStream: InputStream
+        get() = process?.inputStream
+            ?: throw IllegalStateException("Process not started yet")
+
+    override val outputStream: OutputStream
+        get() = process?.outputStream
+            ?: throw IllegalStateException("Process not started yet")
+
+    override fun start() {
+        if (process != null) return // already started
+
+        val pb = if (alpine) {
+            val initHost = localBinDir().child("init-host")
+            ProcessBuilder("sh", "-c",initHost.absolutePath, command.joinToString(" "))
+        } else {
+            ProcessBuilder(*command)
+        }
+
+        pb.redirectError(ProcessBuilder.Redirect.INHERIT)
+        process = pb.start()
+    }
+
+    override fun close() {
+        try {
+            process?.destroy()
+        } catch (_: Exception) {
+        } finally {
+            process = null
+        }
+    }
+}
