f228c5f0396e3e1966908468205c8e59a9a94510
RohitKushvaha01 <oldisg131@gmail.com>
2025-09-07 16:24:16 +0530

    fix: disable or warn when running non-native filetypes

--- core/main/src/main/java/com/rk/runner/runners/UniversalRunner.kt
+++ core/main/src/main/java/com/rk/runner/runners/UniversalRunner.kt
@@ -1,21 +1,28 @@
 package com.rk.runner.runners
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.os.Environment
 import com.rk.file.FileObject
+import com.rk.file.FileWrapper
 import com.rk.file.child
 import com.rk.file.createFileIfNot
 import com.rk.file.localBinDir
 import com.rk.file.localDir
 import com.rk.libcommons.TerminalCommand
 import com.rk.libcommons.application
+import com.rk.libcommons.dialog
 import com.rk.resources.drawables
 import com.rk.resources.getDrawable
+import com.rk.resources.getString
+import com.rk.resources.strings
 import com.rk.runner.RunnerImpl
 import com.rk.terminal.launchInternalTerminal
 import com.rk.xededitor.ui.screens.terminal.stat
 
 class UniversalRunner : RunnerImpl(){
+    @SuppressLint("SdCardPath")
     override fun run(context: Context, fileObject: FileObject) {
         with(localBinDir().child("universal_runner")){
             if (exists().not()){
@@ -23,6 +30,16 @@ class UniversalRunner : RunnerImpl(){
                 writeText(application!!.assets.open("terminal/universal_runner.sh").bufferedReader()
                     .use { it.readText() })
             }
+        }
+
+        if (fileObject !is FileWrapper){
+            dialog(title = strings.attention.getString(), msg = strings.non_native_filetype.getString(), onOk = {})
+            return
+        }
+
+        val path = fileObject.getAbsolutePath()
+        if (path.startsWith("/sdcard") || path.startsWith("/storage/") || path.startsWith(Environment. getExternalStorageDirectory().absolutePath)){
+            dialog(title = strings.attention.getString(), msg = strings.sdcard_filetype.getString(), onOk = {})
         }
 
         launchInternalTerminal(context, terminalCommand = TerminalCommand(
