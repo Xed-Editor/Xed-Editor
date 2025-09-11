610f3014a4f1a14d76224350df82f60223ae592b
RohitKushvaha01 <oldisg131@gmail.com>
2025-09-11 15:23:32 +0530

    fix: showing parent name in first tab

--- core/main/src/main/java/com/rk/tabs/EditorTab.kt
+++ core/main/src/main/java/com/rk/tabs/EditorTab.kt
@@ -110,7 +110,7 @@ class EditorTab(
     override var tabTitle: MutableState<String> = mutableStateOf(file.getName()).also {
         scope.launch{
             val parent = file.getParentFile()
-            if (viewModel.tabs.any { it.tabTitle.value == tabTitle.value } && parent != null){
+            if (viewModel.tabs.any { it.tabTitle.value == tabTitle.value && it != this@EditorTab } && parent != null){
                 tabTitle.value = "${parent.getName()}/${tabTitle.value}"
             }
         }
