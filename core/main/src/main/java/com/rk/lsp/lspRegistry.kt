420442789ed48ae9643fe2d446edef27a5a6fe7c
RohitKushvaha01 <oldisg131@gmail.com>
2025-11-01 15:45:44 +0530

    fix: downloading rootfs everytime when opening terminal

--- core/main/src/main/java/com/rk/lsp/lspRegistry.kt
+++ core/main/src/main/java/com/rk/lsp/lspRegistry.kt
@@ -4,10 +4,8 @@ import com.rk.lsp.servers.Bash
 import com.rk.lsp.servers.CSS
 import com.rk.lsp.servers.HTML
 import com.rk.lsp.servers.JSON
-import com.rk.lsp.servers.LUA
 import com.rk.lsp.servers.Python
 import com.rk.lsp.servers.TypeScript
-import com.rk.lsp.servers.XML
 
 val lspRegistry = listOf(
     Python(),
@@ -16,6 +14,4 @@ val lspRegistry = listOf(
     TypeScript(),
     JSON(),
     Bash(),
-    XML(),
-    LUA()
 )
