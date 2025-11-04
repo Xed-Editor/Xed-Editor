ee3ef157606c72bb5041e1d834fbf378aec59e2d
RohitKushvaha01 <oldisg131@gmail.com>
2025-11-04 15:01:02 +0530

    feat: disable annoying pycodestyle warnings

--- core/main/src/main/java/com/rk/lsp/servers/Python.kt
+++ core/main/src/main/java/com/rk/lsp/servers/Python.kt
@@ -49,6 +49,23 @@ class Python() : BaseLspServer() {
         return supportedExtensions.contains(file.getName().substringAfterLast("."))
     }
     override fun getInitializationOptions(uri: URI?): Any? {
-        return null
+        return mapOf(
+            "plugins" to mapOf(
+                "pycodestyle" to mapOf(
+                    "enabled" to true,
+                    "ignore" to listOf(
+                        "E501",  // line too long
+                        "W291",  // trailing whitespace
+                        "W293",  // blank line contains whitespace
+                        "E301",  // expected 1 blank line
+                        "E302",  // expected 2 blank lines
+                        "E303",  // too many blank lines
+                        "E304",  // blank line after function decorator
+                        "W391"   // blank line at end of file
+                    ),
+                    "maxLineLength" to 999
+                )
+            )
+        )
     }
 }
