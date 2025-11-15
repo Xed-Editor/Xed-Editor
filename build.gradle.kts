690881b25857a24593b51cfbcb9a06aeae94180f
KonerDev <105141148+KonerDev@users.noreply.github.com>
2025-11-15 20:32:29 +0100

    chore: improve `pre-commit` hook and increase width to 140

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ subprojects {
     plugins.withId(rootProject.libs.plugins.ktfmt.get().pluginId) {
         configure<com.ncorti.ktfmt.gradle.KtfmtExtension> {
             kotlinLangStyle()
+            maxWidth.set(140)
         }
     }
 }
