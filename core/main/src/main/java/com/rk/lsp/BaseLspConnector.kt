e070f84c2e4dd3f2005cc8d959dce4a154752321
KonerDev <105141148+KonerDev@users.noreply.github.com>
2025-11-19 03:01:46 +0100

    feat: inlay hints & dialog/popup syntax highlighting (#963)
    feat(main): add LSP inlay hints, editor markdown highlighting, syntax highlighting in findings dialog and improve LSP terminal unavailability warning

--- core/main/src/main/java/com/rk/lsp/BaseLspConnector.kt
+++ core/main/src/main/java/com/rk/lsp/BaseLspConnector.kt
@@ -141,6 +141,7 @@ class BaseLspConnector(
                 project!!.getOrCreateEditor(fileObject.getAbsolutePath()).apply {
                     wrapperLanguage = TextMateLanguage.create(textMateScope,false)
                     editor = codeEditor
+                    isEnableInlayHint = true
                 }
             }
 
