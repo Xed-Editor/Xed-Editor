d18b8df31842dd1a6d55ada615686707ed65fecf
RohitKushvaha01 <oldisg131@gmail.com>
2025-12-10 10:42:59 +0530

    update

--- plugin-sdk/setup.sh
+++ plugin-sdk/setup.sh
@@ -3,12 +3,17 @@
 DEST="libs"
 mkdir -p "$DEST"
 
-# Find all full.jar files inside build/intermediates/full_jar
 find .. -name classes.jar \
   -print0 | while IFS= read -r -d '' jar; do
-    # extract module name (folder before /build/)
     module=$(echo "$jar" | sed 's/.*\/\([^/]*\)\/build\/.*/\1/')
     outfile="$DEST/${module}.jar"
     cp "$jar" "$outfile"
+done
+
+find .. -type f -name .ignore_plugin_sdk -print0 | while IFS= read -r -d '' file; do
+  file_to_remove="$DEST/$(basename "$(dirname "$file")").jar"
+  if [ -f "$file_to_remove" ]; then
+      rm "$file_to_remove"
+  fi
 done
 
