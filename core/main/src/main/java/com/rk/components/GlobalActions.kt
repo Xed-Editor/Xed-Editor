32926c1e8b58843ebd01b145c7cb6e1c90a21a39
Rohit Kushvaha <oldisg131@gmail.com>
2025-12-16 00:38:47 +0000

    Revert "Project wide search and replace implementation (#1019)"
    This reverts commit 015e8a9081ad373cac27d0ac64ff785e96c35b05.

--- core/main/src/main/java/com/rk/components/GlobalActions.kt
+++ core/main/src/main/java/com/rk/components/GlobalActions.kt
@@ -53,8 +53,6 @@ import kotlinx.coroutines.launch
 var addDialog by mutableStateOf(false)
 var fileSearchDialog by mutableStateOf(false)
 var codeSearchDialog by mutableStateOf(false)
-var projectReplaceDialog by mutableStateOf(false)
-var projectSearchReplaceDialog by mutableStateOf(false)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -106,21 +104,6 @@ fun RowScope.GlobalActions(viewModel: MainViewModel) {
             viewModel = viewModel,
             projectFile = (currentTab as FileTreeTab).root,
             onFinish = { codeSearchDialog = false },
-        )
-    }
-
-    if (projectReplaceDialog && currentTab is FileTreeTab) {
-        ProjectReplaceDialog(
-            projectRoot = (currentTab as FileTreeTab).root,
-            onFinish = { projectReplaceDialog = false },
-        )
-    }
-
-    if (projectSearchReplaceDialog && currentTab is FileTreeTab) {
-        ProjectSearchReplaceDialog(
-            viewModel = viewModel,
-            projectFile = (currentTab as FileTreeTab).root,
-            onFinish = { projectSearchReplaceDialog = false },
         )
     }
 
