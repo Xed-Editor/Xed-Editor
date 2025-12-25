d8d23ea40b949be7ed85168116592654367caccb
RohitKushvaha01 <oldisg131@gmail.com>
2025-12-25 13:21:34 +0530

    feat: issues.yml

--- .github/scripts/improve-title-gemini.js
+++ .github/scripts/improve-title-gemini.js
@@ -36,7 +36,7 @@ async function callGemini(prompt) {
 
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-1.5-pro", // or gemini-1.5-pro / flash
+      model: "gemini-2.5-flash-lite",
       contents: prompt,
     });
 
