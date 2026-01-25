3ebc1ec5d9707f08593a34735b30637a681b2f2f
Andro_Dev <andromaster436@gmail.com>
2026-01-25 19:03:32 +0300

    feat: git settings screen

--- core/main/src/main/java/com/rk/activities/settings/SettingsRoutes.kt
+++ core/main/src/main/java/com/rk/activities/settings/SettingsRoutes.kt
@@ -38,4 +38,6 @@ sealed class SettingsRoutes(val route: String) {
     data object Themes : SettingsRoutes("theme")
 
     data object LspSettings : SettingsRoutes("lsp_settings")
+
+    data object Git : SettingsRoutes("git")
 }
---
+++ core/main/src/main/java/com/rk/settings/git/GitSettings.kt
@@ -0,0 +1,13 @@
+package com.rk.settings.git
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.rk.components.compose.preferences.base.PreferenceLayout
+import com.rk.resources.strings
+
+@Composable
+fun GitSettings() {
+    PreferenceLayout(label = stringResource(id = strings.git)) {
+        // todo
+    }
+}
