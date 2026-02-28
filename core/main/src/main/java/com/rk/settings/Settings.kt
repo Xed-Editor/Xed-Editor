c7301b65aafe5554032c3bf1ce14c1420eb74467
KonerDev <105141148+KonerDev@users.noreply.github.com>
2026-02-28 16:01:05 +0100

    fix: fix editor not loading content

--- core/main/src/main/java/com/rk/settings/Settings.kt
+++ core/main/src/main/java/com/rk/settings/Settings.kt
@@ -59,8 +59,8 @@ object ReactiveSettings {
 
 // NOTE: USE snake_case FOR KEYS!
 object Settings {
-    var disable_bin_files by CachedPreference("disable_bin_files", false)
-    var oom_prediction by CachedPreference("disable_oom_prediction", false)
+    var detect_bin_files by CachedPreference("detect_bin_files", true)
+    var oom_prediction by CachedPreference("disable_oom_prediction", true)
 
     var read_only_default by CachedPreference("read_only_default", false)
     var shown_disclaimer by CachedPreference("shown_disclaimer", false)
