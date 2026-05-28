f7be7bf68a86311f40238e9519931e82b22c51ac
algospider <mohanrajauria@gmail.com>
2026-05-29 00:28:16 +0530

    updating ui

--- core/main/src/main/java/com/rk/ai/UnifiedCommandBar.kt
+++ core/main/src/main/java/com/rk/ai/UnifiedCommandBar.kt
@@ -1,6 +1,7 @@
 package com.rk.ai
 
 import androidx.compose.animation.*
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
