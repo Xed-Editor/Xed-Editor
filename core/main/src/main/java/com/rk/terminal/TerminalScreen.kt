b6aced52c868b5c3b6061d5369a4d0483cc468f4
algospider <mohanrajauria@gmail.com>
2026-05-29 05:36:49 +0530

    fix: remove incorrect `weight` import in TerminalScreen.kt

--- core/main/src/main/java/com/rk/terminal/TerminalScreen.kt
+++ core/main/src/main/java/com/rk/terminal/TerminalScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
