243a6aafebe6a3342a8184fee46a761cd03bd63d
KonerDev <105141148+KonerDev@users.noreply.github.com>
2026-08-30 21:54:18 +0200

    feat: switch TypeScript LSP server from typescript-language-server to tsc (TypeScript 7)
    - Change the LSP server name to `tsc` and target the `typescript` package for update checks
    - Update connection configuration to run `/usr/bin/tsc` with `--lsp` and `--stdio` arguments
    - Update the shell script to install and update `typescript@latest` instead of `typescript-language-server`
    - Bump application `versionCode` to 106

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         targetSdk = 37
 
         // versioning
-        versionCode = 105
+        versionCode = 106
         versionName = "3.4.4"
         vectorDrawables { useSupportLibrary = true }
     }
